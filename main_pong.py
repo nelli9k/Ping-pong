0ecbcfbdae425a2509f3194ca3e8653992627c25
Walkoszes <130684485+Walkoszes@users.noreply.github.com>
2023-04-28 16:26:59 +0300

    Add files via upload
    Two rockets can move up and down. The right rocket  move up with button "W" and move down by button "S". The left rocket move up with the button "↑" and move down by the button "↓"

--- main_pong.py
+++ main_pong.py
@@ -4,6 +4,7 @@ from time import time as timer
 background = "background.png"
 ball = "ball.png"
 tennis_r = "tennis_r.png"
+
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, x, y, sizex, sizey, player_speed):
         sprite.Sprite.__init__(self)
@@ -16,9 +17,25 @@ class GameSprite(sprite.Sprite):
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
+class Player(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y = self.rect.y - self.speed
+        if keys[K_s] and self.rect.y < 300:
+            self.rect.y = self.rect.y + self.speed
+
+class Player2(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y = self.rect.y - self.speed
+        if keys[K_DOWN] and self.rect.y < 300:
+            self.rect.y = self.rect.y + self.speed
+
 ball = GameSprite(ball, 200, 200, 50, 50, 0)
-tenis_rocket = GameSprite(tennis_r, 50, 200, 75, 200, 0)
-tenis_rocket2 = GameSprite(tennis_r, 550, 200, 75, 200, 0)
+tenis_rocket = Player(tennis_r, 50, 200, 75, 200, 12)
+tenis_rocket2 = Player2(tennis_r, 550, 200, 75, 200, 12)
 window = display.set_mode((700,500))
 display.set_caption("Shooter")
 background = transform.scale(image.load(background), (700,500))
@@ -34,7 +51,9 @@ while run:
         window.blit(background, (0,0))
 
         ball.reset()
+        tenis_rocket.update()
         tenis_rocket.reset()
+        tenis_rocket2.update()
         tenis_rocket2.reset()
 
         display.update()
